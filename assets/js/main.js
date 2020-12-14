2561cbb94a6a485d3b57618a945644959dcf081b
Hridoy <hridoyhalderpartho@gmail.com>
2020-12-14 14:05:18 +0600

    banner section and brand section compleate

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,30 @@
 (function ($) {
 	"use strict";
 
-
+    //brand_slider
+    $('.brand-slider-active').slick({
+		infinite: true,
+		slidesToShow: 5,
+		slidesToScroll: 1,
+		arrows: false,
+		autoplay: true,
+		autoplaySpeed: 1200,
+		responsive: [
+			{
+				breakpoint: 1024,
+				settings: {
+					slidesToShow: 3,
+				},
+			},
+			{
+				breakpoint: 480,
+				settings: {
+					centerMode: true,
+					centerPadding: '10px',
+					slidesToShow: 2,
+				},
+			},
+		],
+    });
 
 })(jQuery);
