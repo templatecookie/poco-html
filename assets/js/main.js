31fff76d1d541c8d57253adb4516467d3a789f8f
Hridoy <hridoyhalderpartho@gmail.com>
2020-12-15 14:28:20 +0600

    add magnify popup video

--- assets/js/main.js
+++ assets/js/main.js
@@ -52,6 +52,18 @@
 				},
 			},
 		],
-    });
+	});
+	
+	//Popup Video
+	$(function() {
+		$('.popup-youtube').magnificPopup({
+			disableOn: 700,
+			type: 'iframe',
+			mainClass: 'mfp-fade',
+			removalDelay: 160,
+			preloader: false,
+			fixedContentPos: false
+		});
+	});
 
 })(jQuery);
