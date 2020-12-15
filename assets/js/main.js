c8a1ac7a4948833e48ae66c186f1387b01780c48
Hridoy <hridoyhalderpartho@gmail.com>
2020-12-15 13:07:54 +0600

    add some bug fix and compleate testimonial section

--- assets/js/main.js
+++ assets/js/main.js
@@ -25,6 +25,33 @@
 				},
 			},
 		],
+	});
+	
+    //testimonial_slider
+    $('.testimonial-active').slick({
+		infinite: true,
+		slidesToShow: 3,
+		slidesToScroll: 1,
+		arrows: false,
+		autoplay: true,
+		arrows: true,
+		autoplaySpeed: 2000,
+		prevArrow: '<div class="prev-arrow"><img class="img-fluid" src="assets/img/icon/f.svg" alt="icon"></div>',
+		nextArrow: '<div class="next-arrow"><img class="img-fluid" src="assets/img/icon/right.svg" alt="icon"></div>',
+		responsive: [
+			{
+				breakpoint: 1024,
+				settings: {
+					slidesToShow: 3,
+				},
+			},
+			{
+				breakpoint: 480,
+				settings: {
+					slidesToShow: 1,
+				},
+			},
+		],
     });
 
 })(jQuery);
